77ee1c74dd7dea6f6afd0e63406df958da4eba39
K1ngKP <131380727+K1ngKP@users.noreply.github.com>
2024-07-27 22:27:01 +0530

    Time: 31 ms (92.99%) | Memory: 16.5 MB (36.65%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,9 +4,4 @@ class Solution:
             for j in range(i, len(nums)):
                 nums[i][j], nums[j][i] = nums[j][i], nums[i][j]
         for i in range(0, len(nums)):
-            for j in range(0, len(nums)//2):
-                nums[i][j], nums[i][len(nums)-j-1] = nums[i][len(nums)-j-1], nums[i][j]
-
-
-
-        
+            nums[i].reverse()    
